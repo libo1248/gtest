855ada204a858258ada0c6d8e1d51df73c304672
libo1248 <libo1248@gmail.com>
2021-02-26 18:55:29 +0800

    add cmd annotation

--- string_byte.go
+++ string_byte.go
@@ -14,3 +14,11 @@ func main() {
 	c := []byte(a)
 	fmt.Println(c)
 }
+
+//# go build -gcflags "-N -l" string_byte.go
+//# gdb string_byte
+//(gdb) b 15
+//(gdb) info locals
+//(gdb) ptype a
+//(gdb) ptype b
+//(gdb) ptype c
